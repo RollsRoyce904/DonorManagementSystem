373cda9ec8e4c67c6607fe02cacb835d0b9b47c0
rollsroyce94 <slip4665@nefcom.net>
2017-03-28 11:16:54 -0400

    ok

--- testDMS/Models/DonorViewModel.cs
+++ testDMS/Models/DonorViewModel.cs
@@ -13,5 +13,6 @@ namespace testDMS.Models
         public int PageNumber { get; set; }
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
+        public int ItemNumber { get; set; }
     }
 }
