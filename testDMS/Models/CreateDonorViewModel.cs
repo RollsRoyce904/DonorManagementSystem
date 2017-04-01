6431a8ee077414b05631bafe86a94007c35c5e1b
rollsroyce94 <slip4665@nefcom.net>
2017-04-01 14:05:27 -0400

    I CHANGED ALOT

--- testDMS/Models/CreateDonorViewModel.cs
+++ testDMS/Models/CreateDonorViewModel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace testDMS.Models
+﻿namespace testDMS.Models
 {
     public class CreateDonorViewModel
     {
-        public DONOR donor { get; set; }   
+        public DONOR donor { get; set; }
         //public COMPANY company { get; set; }
         public CONTACT contact { get; set; }
         public IDENTITYMARKER identityMarker { get; set; }
